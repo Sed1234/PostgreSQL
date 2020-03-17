dc83f297f6bde3abc853fcefaa5984b27d1aecfd
Sergey <38430251+Sed1234@users.noreply.github.com>
2020-03-18 01:05:50 +0300

    Initial commit

---
+++ articles_tags DB.sql
@@ -0,0 +1,24 @@
+﻿create table articles
+(
+ID integer primary key,
+article character varying(100) not null
+);
+
+create table tags
+(
+ID integer primary key,
+tag character varying(40) not null,
+ID_article integer not null,
+FOREIGN KEY (ID_article) REFERENCES articles(id)
+);
+
+
+INSERT INTO articles VALUES (1, 'Европа разваливается?');
+INSERT INTO tags VALUES (1, 'Европа',1);
+INSERT INTO tags VALUES (2, 'Развал',1);
+INSERT INTO articles VALUES (2, 'Как христианство стало господствующей религией');
+INSERT INTO tags VALUES (3, 'Религия',2);
+INSERT INTO tags VALUES (4, 'христианство',2);
+INSERT INTO tags VALUES (5, 'господствующей',2);
+INSERT INTO articles VALUES (3, 'Кин-Дза-Дза');
+
---
+++ articles_tagsScripts.sql
@@ -0,0 +1,4 @@
+﻿select a.article, t.tag
+from articles a 
+left outer join  tags t
+on a.id = t.id_article
